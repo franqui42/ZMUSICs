f45b08501900b133c60b3bae7be18e1aa4f3e122
frank42 <dazafrank42@gmail.com>
2021-05-22 12:42:33 -0500

    despliegue

--- ZMUSICs/settings.py
+++ ZMUSICs/settings.py
@@ -78,27 +78,27 @@ WSGI_APPLICATION = 'ZMUSICs.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'd2rr13k37h4gdg',
-#         'USER': 'ebqhjaisdiqwlf',
-#         'Password': '9d968e29e39b4b6c2dc8c6aa423bf90063ee9ef1c2432b2c2a4789fde7729dcf',
-#         'HOST': 'ec2-54-152-185-191.compute-1.amazonaws.com',
-#         'PORT': '5432',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'ZMUSICs',
-        'USER': 'root',
-        'Password': '',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'd2rr13k37h4gdg',
+        'USER': 'ebqhjaisdiqwlf',
+        'Password': '9d968e29e39b4b6c2dc8c6aa423bf90063ee9ef1c2432b2c2a4789fde7729dcf',
+        'HOST': 'ec2-54-152-185-191.compute-1.amazonaws.com',
+        'PORT': '5432',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'ZMUSICs',
+#         'USER': 'root',
+#         'Password': '',
+#         'HOST': '127.0.0.1',
+#         'PORT': '3306',
+#     }
+# }
 
 
 # Password validation
