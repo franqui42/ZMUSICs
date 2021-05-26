fdf88e066575031633e60e949802f55ce361661f
frank42 <dazafrank42@gmail.com>
2021-05-26 11:07:52 -0500

    seting

--- ZMUSICs/settings.py
+++ ZMUSICs/settings.py
@@ -94,7 +94,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'd2rr13k37h4gdg',
         'USER': 'ebqhjaisdiqwlf',
-        'Password': '9d968e29e39b4b6c2dc8c6aa423bf90063ee9ef1c2432b2c2a4789fde7729dcf',
+        'PASSWORD': '9d968e29e39b4b6c2dc8c6aa423bf90063ee9ef1c2432b2c2a4789fde7729dcf',
         'HOST': 'ec2-54-152-185-191.compute-1.amazonaws.com',
         'PORT': '5432',
     }
